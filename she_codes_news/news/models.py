4aa11faf453afe16fae9d6008c56ea99e959d539
Rachaeldagge <51501849+Rachaeldagge@users.noreply.github.com>
2020-08-22 11:30:49 +1000

    part 1 - Saturday - forms

--- she_codes_news/news/models.py
+++ she_codes_news/news/models.py
@@ -6,3 +6,4 @@ class NewsStory(models.Model):
     author = models.CharField(max_length=200)
     pub_date = models.DateTimeField()
     content = models.TextField()
+    image_url = models.CharField(max_length=200, default='https://helpx.adobe.com/content/dam/help/en/stock/how-to/visual-reverse-image-search/jcr_content/main-pars/image/visual-reverse-image-search-v2_intro.jpg')
